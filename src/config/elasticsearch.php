0773d32571f83715c53f8b7726bda227058cd75b
Nurdaulet <nurik9293709@gmail.com>
2020-03-23 21:28:26 +0600

    search params

--- src/config/elasticsearch.php
+++ src/config/elasticsearch.php
@@ -5,4 +5,11 @@ return [
         "localhost:9200",
     ],
     'enabled' => env('ELSTIC_ENABLED', false),
+
+    'search_params' => [
+        'fuzziness' => '6',
+        'fuzzy_max_expansions' => 100,
+        'fuzzy_prefix_length' => 10,
+        'minimum_should_match' => '0'
+    ]
 ];
