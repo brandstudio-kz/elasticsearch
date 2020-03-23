907090fe64dc342d6b747104249e8bf845ab3116
Nurdaulet <nurik9293709@gmail.com>
2020-03-23 21:12:22 +0600

    orderBy + pagination

--- src/Builder.php
+++ src/Builder.php
@@ -8,28 +8,37 @@ class Builder
     protected $client;
     protected $model;
 
-    protected $query_string;
     protected $must = [];
     protected $must_not = [];
     protected $should = [];
     protected $filter = [];
+    protected $query = [];
 
     public function __construct(string $model, string $q = null)
     {
         $this->model = $model;
         $this->client = resolve(ElasticClient::class);
 
-        if (is_string($q)) {
-            $this->query_string = $q;
-        }
+        $this->query = [
+            'index' => $this->model::getIndexName(),
+            'body' => [
+                'query' => [
+                    'query_string' => [
+                        'query' => $q,
+                        'fuzziness' => '6',
+                        'fuzzy_max_expansions' => 100,
+                        'fuzzy_prefix_length' => 10,
+                        'minimum_should_match' => '0'
+                    ]
+                ],
+            ],
+        ];
     }
 
-    public function get()
+    public function get($is_paginate = false)
     {
-        $params = $this->getQuery();
-        $response = $this->client->search($params);
-
-        return array_map(
+        $response = $this->client->search($this->query);
+        $data = array_map(
             function($item) {
                 $model = new $this->model;
                 $model->prepareToResponse($item['_source'] ?? []);
@@ -37,11 +46,33 @@ class Builder
                 return $model;
             }, $response['hits']['hits']
         );
+
+        if ($is_paginate) {
+            return [
+                'total' => $response['hits']['total']['value'],
+                'data' => $data,
+            ];
+        }
+
+        return $data;
     }
 
-    public function paginate()
+    public function paginate($per_page = 10)
     {
-        return $this->get();
+        $this->query['size'] = $per_page;
+        $this->query['from'] = $per_page * ((request()->page ?? 1) - 1);
+        $data = $this->get(true);
+        $data['per_page'] = $per_page;
+        $data['last_page'] = ceil($data['total']/$per_page);
+        return $data;
+    }
+
+    public function orderBy(string $field, string $order) {
+        if (!isset($this->query['sort'])) {
+            $this->query['sort'] = [];
+        }
+        $this->query['sort'][] = "{$field}:{$order}";
+        return $this;
     }
 
     public function where() : self
@@ -63,24 +94,6 @@ class Builder
     {
         $this->filter['term'][$field] = $value;
         return $this;
-    }
-
-    protected function getQuery() : array
-    {
-        return [
-            'index' => $this->model::getIndexName(),
-            'body' => [
-                'query' => [
-                    'query_string' => [
-                        'query' => $this->query_string,
-                        'fuzziness' => '6',
-                        'fuzzy_max_expansions' => 100,
-                        'fuzzy_prefix_length' => 10,
-                        'minimum_should_match' => '1'
-                    ]
-                ],
-            ],
-        ];
     }
 
 }
