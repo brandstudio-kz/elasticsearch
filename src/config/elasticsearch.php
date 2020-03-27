4cf042c91ae691ee7a6ee4b09bcb2dc2e91da175
Nurdaulet <nurik9293709@gmail.com>
2020-03-27 23:27:33 +0600

    Update elasticsearch.php

--- src/config/elasticsearch.php
+++ src/config/elasticsearch.php
@@ -5,11 +5,4 @@ return [
         "localhost:9200",
     ],
     'enabled' => env('ELSTIC_ENABLED', false),
-
-    'search_params' => [
-        'fuzziness' => '6',
-        'fuzzy_max_expansions' => 100,
-        'fuzzy_prefix_length' => 10,
-        'minimum_should_match' => '0'
-    ]
 ];
